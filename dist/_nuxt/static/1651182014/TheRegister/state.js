dd4a4a5f4604de2a4010433262b6905826d3a6df
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-05-04 14:01:26 -0400

    :rocket: deploy 3.0

--- dist/_nuxt/static/1651182014/TheRegister/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651182014",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},state:{places:[],popularPlaces:[],cities:[],reviews:[]},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null}}}}("\u002FTheRegister"));
