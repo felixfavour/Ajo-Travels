8a0e09a04927ddaf685e63c64ca17bc92d7104f7
SualaDev <81395134+SualaDev@users.noreply.github.com>
2022-05-05 21:25:01 +0100

    merge conflict fix

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,20 @@
 <template>
   <main>
+    <div class="main-container">
+      <NavBar />
+      <div class="container">
+        <div class="background-images">
+          <div class="phones" />
+          <div class="store" />
+        </div>
+        <Details />
+        <Cards />
+        <Review />
+      </div>
+      <Footer />
+    </div>
     <div class="logo">
-      <img src="../assets/img/ajo-logo.png" alt="" />
+      <img src="../assets/img/ajo-logo.png" alt="">
     </div>
     <div class="title">
       <!-- <img src="../assets/img/ajo-text.png" alt="" /> -->
@@ -21,14 +34,41 @@
 </template>
 
 <script>
-import TheButton from "~/components/TheButton.vue";
+import TheButton from '~/components/TheButton.vue'
 export default {
-  name: "IndexPage",
-  transition: "start",
+  name: 'IndexPage',
   components: { TheButton },
-};
+  transition: 'start'
+}
 </script>
 <style lang="scss" scoped>
+.main-container{
+  position: relative;
+}
+.container{
+  max-width: 1440px;
+  margin: 0 auto;
+}
+.phones{
+  background-image: url(~assets/images/phone.png);
+  position: absolute;
+  width: 580.49px;
+  height: 701.75px;
+  top: 443px;
+  left: 0;
+  right:0;
+  margin: auto;
+}
+.store{
+  background-image: url(~assets/images/stores.png);
+  position: absolute;
+  width: 535px;
+  height: 84px;
+  top: 1241px;
+  left: 0;
+  right: 0;
+  margin: auto;
+}
 @media screen and (max-width: 428px) {
   main {
     display: flex;
