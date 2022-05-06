426d1d59c50c4e86d5bae16e3d9e15c59b306ea3
Chukwuemeka Mba <76162087+Chukwuemeka-Mba@users.noreply.github.com>
2022-05-06 06:13:17 -0400

    :building_construction: deploy 4.0

--- dist/_nuxt/static/1651759152/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1651759152",layout:"default",error:a,state:{places:[],popularPlaces:[],cities:[],reviews:[],user:[],placeDetail:[],userDetails:[],images:[],similarPlaces:[]},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null,"\u002F"));
